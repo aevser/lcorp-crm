d76b7b33515fc369465eb6a0884727429ba5a11b
Andrey <aevsergeevich@gmail.com>
2023-12-12 15:30:16 +0400

    Изменений миграций

--- database/migrations/2023_12_12_074700_create_roles_table.php
+++ database/migrations/2023_12_12_074700_create_roles_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(\App\Models\User::class, column: 'user_id')
+                ->constrained()
+                ->onDelete('cascade');
             $table->boolean('create_projects');
             $table->boolean('manage_users');
             $table->boolean('manage_permissions');
