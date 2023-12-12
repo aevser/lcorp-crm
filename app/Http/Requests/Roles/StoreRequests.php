7adf6e8f0ec8219b84707e161fb92b227dc0e875
Andrey <aevsergeevich@gmail.com>
2023-12-12 15:23:27 +0400

    Валидация ролей

---
+++ app/Http/Requests/Roles/StoreRequests.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\Roles;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRequests extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'create_projects' => 'boolean',
+            'manage_users' => 'boolean',
+            'manage_permissions' => 'boolean',
+            'edit_projects' => 'boolean',
+            'global_settings' => 'boolean',
+        ];
+    }
+}
