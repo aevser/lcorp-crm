4c88310383ed3d3bf3ec4da8271efeb354916232
Andrey <aevsergeevich@gmail.com>
2023-12-12 14:56:46 +0400

    Валидация разрешений

---
+++ app/Http/Requests/Permissions/UpdateRequests.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Requests\Permissions;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRequests extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'project_id' => 'required|exists:projects,id',
+            'user_id' => 'required|exists:users,id',
+            'fields' => 'array',
+            [
+                'surname' => 'nullable|string',
+                'patronymic' => 'nullable|string',
+                'full_name' => 'nullable|string',
+                'phone' => 'nullable|string',
+                'entries' => 'nullable|string',
+                'email' => 'nullable|email',
+                'cost' => 'nullable|string',
+                'comment' => 'nullable|string',
+                'city' => 'nullable|string',
+                'region' => 'nullable|string',
+                'manual_region' => 'nullable|string',
+                'manual_city' => 'nullable|string',
+                'ip' => 'nullable|ip',
+                'referrer' => 'nullable|string',
+                'source' => 'nullable|string',
+                'utm' => 'nullable|string',
+                'utm_medium' => 'nullable|string',
+                'utm_source' => 'nullable|string',
+                'utm_campaign' => 'nullable|string',
+                'utm_content' => 'nullable|string',
+                'utm_term' => 'nullable|string',
+                'host' => 'nullable|string',
+            ],
+            'manage_leads' => 'boolean',
+            'export_data' => 'boolean',
+            'manage_permissions' => 'boolean',
+            'manage_settings' => 'boolean',
+            'manage_project' => 'boolean'
+        ];
+    }
+}
