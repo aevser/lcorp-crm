aed3d88d30e69c46d377497c07ea3aaf99fab250
Andrey <aevsergeevich@gmail.com>
2023-12-14 16:44:24 +0400

    Добавлен маршрут авторизации

--- routes/api.php
+++ routes/api.php
@@ -4,16 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
@@ -25,3 +16,9 @@ Route::apiResource('permissions', Api\Admin\PermissionsController::class)->only(
 Route::apiResource('roles', Api\Admin\RolesController::class)->only([
     'index', 'show', 'store', 'update', 'destroy'
 ]);
+Route::apiResource('projects', Api\Admin\ProjectController::class)->only([
+    'index', 'store'
+]);
+Route::post('/login', [Api\AuthController::class, 'login'])->name('login');
+
+Route::post('/logout', [Api\AuthController::class, 'logout'])->name('logout');
