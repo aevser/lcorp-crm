410447cf643f6ea8ab59459afb8aba0d0c8a0e8c
Andrey <aevsergeevich@gmail.com>
2023-12-14 23:06:18 +0400

    Добавлен middleware и ресурс для проектов

--- routes/api.php
+++ routes/api.php
@@ -6,19 +6,18 @@ use App\Http\Controllers\Api;
 
 
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function(){
+    Route::apiResource('projects', Api\Admin\ProjectController::class)->only([
+        'index', 'store'
+    ]);
 });
-
 Route::apiResource('permissions', Api\Admin\PermissionsController::class)->only([
     'index', 'show', 'store', 'update', 'destroy'
 ]);
 Route::apiResource('roles', Api\Admin\RolesController::class)->only([
     'index', 'show', 'store', 'update', 'destroy'
 ]);
-Route::apiResource('projects', Api\Admin\ProjectController::class)->only([
-    'index', 'store'
-]);
+
 Route::post('/login', [Api\AuthController::class, 'login'])->name('login');
 
 Route::post('/logout', [Api\AuthController::class, 'logout'])->name('logout');
