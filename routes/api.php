d04e06ef64de5845023c9b9c9477ac0479630857
Andrey <aevsergeevich@gmail.com>
2023-12-20 16:23:43 +0400

    Добавлен ресурс для хостов

--- routes/api.php
+++ routes/api.php
@@ -10,14 +10,18 @@ Route::middleware('auth:sanctum')->group(function(){
     Route::apiResource('projects', Api\Admin\ProjectController::class)->only([
         'index', 'store', 'update', 'destroy'
     ]);
+    Route::apiResource('permissions', Api\Admin\PermissionsController::class)->only([
+        'index', 'show', 'store', 'update', 'destroy'
+    ]);
+    Route::apiResource('roles', Api\Admin\RolesController::class)->only([
+        'index', 'show', 'store', 'update', 'destroy'
+    ]);
+    
+    Route::apiResource('hosts', Api\HostController::class)->only([
+        'index', 'store', 'update', 'destroy'
+    ]);
+    Route::post('/logout', [Api\AuthController::class, 'logout'])->name('logout');
 });
-Route::apiResource('permissions', Api\Admin\PermissionsController::class)->only([
-    'index', 'show', 'store', 'update', 'destroy'
-]);
-Route::apiResource('roles', Api\Admin\RolesController::class)->only([
-    'index', 'show', 'store', 'update', 'destroy'
-]);
+
 
 Route::post('/login', [Api\AuthController::class, 'login'])->name('login');
-
-Route::post('/logout', [Api\AuthController::class, 'logout'])->name('logout');
